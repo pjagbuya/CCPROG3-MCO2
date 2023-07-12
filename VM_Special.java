9d1b8d9fdf5979e1b89574825447a0fbcf06b83c
vincerojo <114553075+vincerojo@users.noreply.github.com>
2023-07-12 17:55:03 +0800

    INITIAL COMMIT

---
+++ VM_Special.java
@@ -0,0 +1,21 @@
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.Map;
+import java.text.DecimalFormat;
+
+
+/** This class represents a Special Vending Machine
+  * and its methods and attributes
+  *
+  * @author Paul Josef P. Agbuya
+  * @author Vince Kenneth D. Rojo
+  */
+public class VM_Special extends VM_Regular
+{
+	public VM_Special(String name, 
+					  int nOfSlots, 
+					  int item_max)
+	{
+		super(name, nOfSlots, item_max);
+	}
+}
