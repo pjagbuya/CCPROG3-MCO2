db4708c954ad31023acca489e3d3e8da534b2a8e
vincerojo <114553075+vincerojo@users.noreply.github.com>
2023-08-01 13:24:30 +0800

    javadoc

--- Vending Machine MCO1/src/Models/VM_Special.java
+++ Vending Machine MCO1/src/Models/VM_Special.java
@@ -8,14 +8,20 @@ import java.util.Map;
 import java.text.DecimalFormat;
 
 
-/** This class represents a Special Vending Machine
-  * and its methods and attributes
+/** The class VM_Special represents a Special Vending Machine.
   *
   * @author Paul Josef P. Agbuya
   * @author Vince Kenneth D. Rojo
   */
 public class VM_Special extends VM_Regular
 {
+	/**
+ 	 * Instantiates a Special vending machine and sets its basic characteristics.
+   	 *
+     	 * @param nOfSlots the number of slots in this vending machine
+       	 * @param item_max the maximum number of items per slot
+	 * @change the change tray
+  	 */
 	public VM_Special(String name, 
 					  int nOfSlots, 
 					  int item_max,
@@ -40,27 +46,47 @@ public class VM_Special extends VM_Regular
 				getChange(),
 				specialSlots, 
 				getCustomItems() ));
-	}
+		}
 	
-	
+
+	/**
+ 	 * Sets a new Special operator brain in the vending machine.
+   	 *
+     	 * @param the new Special operator brain
+       	 */
 	public void setSpecialOperator(SpecialSellingOperator specialSellingOperator)
 	{
 		super.setOperator( specialSellingOperator );
 	}
 	
 	
-	
+	/**
+ 	 * Returns the special slots of this vending machine.
+   	 *
+     	 * @return the special slots of this vending machine
+   	 */
 	public VM_Slot[] getSpecialSlots() { return specialSlots; }
-	
+
+
+	/**
+ 	 * Returns the recipe checker of this operator.
+   	 *
+     	 * @return the recipe checker
+     	 */
 	public RecipeChecker getRecipeChecker() { return recipeChecker; }
-	
+
+	/**
+ 	 * Sets the recipe checker of this vending machine.
+   	 *
+     	 * @param the new recipe checker
+     	 */
 	public void setRecipeChecker(RecipeChecker recipeChecker) { this.recipeChecker = recipeChecker; }
 	
 	
 	
 	
 	
-	
+	/** the Special operators's recipe-checking component */
 	private RecipeChecker recipeChecker;
 	/** slots for items that cannot be sold on their own */
 	private VM_Slot[] specialSlots;
